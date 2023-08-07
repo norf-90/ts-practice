9ceed0563e5dd7ddf74c952f2ec791593b78225d
Oleksandr Shmatko <blacknorfolk@gmail.com>
2023-08-07 15:38:52 +0300

    update unit-3

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,39 @@
-const button = document.querySelector('button')!;
+// ============== Arrays ========================
+const skills: string[] = ['Dev', 'DevOps'];
 
-if (button) {
-  button.addEventListener('click', () => {
-    console.log('Click');
-  });
+for (const skill of skills) {
+  console.log(skill.toLocaleLowerCase());
 }
+
+const result = skills
+  .filter((s: string) => s !== 'DevOps')
+  .map(s => s + '!')
+  .reduce((a, b) => a + b);
+console.log(result);
+
+// ============== functions types =======================
+function getFullName(userEntity: { firstName: string; surname: string }): string {
+  return `${userEntity.firstName} ${userEntity.surname}`;
+}
+
+const getFullNameArrow = (firstName: string, surname: string): string => {
+  return `${firstName} ${surname}`;
+};
+
+const user = {
+  firstName: 'Oleksandr',
+  surname: 'Shmatko',
+  city: 'Kyiv',
+  age: 33,
+};
+
+// console.log(getFullName(user));
+// =========================================================================
+
+let revenue: number = 1000;
+let bonust: number = 500;
+let c: string = 'sdf';
+let b: boolean = true;
+
+let res: number = revenue + bonust;
+// console.log(res);
