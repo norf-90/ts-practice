b311499fe1029ef1fad8c03415cf6c5921ddcc8e
Oleksandr Shmatko <blacknorfolk@gmail.com>
2023-08-10 13:30:56 +0300

    unit-3

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,53 @@
+// ========== ENUMS ============================
+
+enum StatusCode {
+  SUCCESS = 1,
+  IN_PROCESS = 'p',
+  FAILED = 'f',
+}
+const result2 = {
+  message: 'Платеж успешен',
+  statusCode: StatusCode.SUCCESS,
+};
+
+// statusCode
+// 1 - success
+// 2 - in progress
+// 3 - rejected
+
+if (result2.statusCode === StatusCode.SUCCESS) {
+}
+
+function action(status: StatusCode) {
+  console.log(status);
+}
+action(StatusCode.SUCCESS);
+action(1);
+// action('p');
+
+const enum Roles {
+  ADMIN,
+  USER,
+}
+
+const res2 = Roles.ADMIN;
+
+// ========Read only=========================
+const skill: readonly [number, string] = [1, 'Dev'];
+// skill[0] = 2;
+
+const skillValues: readonly string[] = ['Dev', 'DevOps'];
+// skills[0] = '';
+// skillValues.push(' ');
+
+const skillValues2: ReadonlyArray<string> = ['str1', 'str2'];
+
+// ==============TUPLE===========================
+// const skill: [number, string] = [1, 'Dev'];
+// const [id, skillName] = skill;
+
+// const arr: [number, string, ...boolean[]] = [1, 'some string', true, true, false];
+
 // ============== Arrays ========================
 const skills: string[] = ['Dev', 'DevOps'];
 
