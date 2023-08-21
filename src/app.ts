a9cc1badd6fab866421539fe584396ec48925e03
Oleksandr Shmatko <blacknorfolk@gmail.com>
2023-08-21 12:35:02 +0300

    static & this

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,69 @@
+// ====== This ======
+
+{
+  class Payment {
+    private date: Date = new Date();
+
+    getDate(this: Payment) {
+      return this.date;
+    }
+
+    getDateArrow = () => {
+      return this.date;
+    };
+  }
+
+  const p = new Payment();
+  const user = {
+    id: 1,
+    paymentDate: p.getDate.bind(p),
+    paymentDateArrow: p.getDateArrow,
+  };
+  console.log(p.getDate());
+  console.log(user.paymentDate());
+  console.log(user.paymentDateArrow());
+
+  class PaymentPersistent extends Payment {
+    save() {
+      return super.getDateArrow();
+    }
+  }
+  console.log(new PaymentPersistent().save());
+
+  console.log(' ');
+}
+
+// ====== Static ======
+{
+  class UserService {
+    static db: any;
+
+    static getUser(id: number) {
+      return UserService.db.findById(id);
+    }
+
+    constructor(id: number) {
+      id;
+    }
+
+    create() {
+      // some code
+      UserService.db;
+    }
+
+    // static block, run after pseudo initialization
+    // don't allow to use async code
+    static {
+      UserService.db = 'sdf';
+    }
+  }
+
+  UserService.db;
+  const inst = new UserService(1);
+  inst.create();
+  // UserService.id;
+}
+
 // ====== Visibility ======
 {
   class Vehicle {
