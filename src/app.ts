5feb4b19e8214eab9c6cb6d9b29d261f9c3b4518
Oleksandr Shmatko <blacknorfolk@gmail.com>
2023-08-23 12:50:04 +0300

    types with generics

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,34 @@
+// ====== Generics in types =======
+
+{
+  function logMiddleware<T>(data: T): T {
+    console.log(data);
+    return data;
+  }
+  function getSplittedHalf<T>(data: Array<T>): Array<T> {
+    const l = data.length / 2;
+    return data.splice(0, l);
+  }
+  const split: <T>(data: Array<T>) => Array<T> = getSplittedHalf;
+  // --------------------------------------------
+  interface ILogLine<T> {
+    timeStamp: Date;
+    data: T;
+  }
+
+  type LogLineType<T> = {
+    timeStamp: Date;
+    data: T;
+  };
+
+  const logline: LogLineType<{ a: number }> = {
+    timeStamp: new Date(),
+    data: {
+      a: 1,
+    },
+  };
+}
+
 // ====== Function with generic ======
 {
   function logMiddleware(data: any): any {
