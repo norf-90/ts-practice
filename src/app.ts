20b765c558af3f1bf6c8cc367b8b3bd38dd11f10
Oleksandr Shmatko <blacknorfolk@gmail.com>
2023-08-23 14:18:33 +0300

    generics enxtending

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,31 @@
+// ====== Generic's limits ======
+{
+  class Vehicle {
+    run: number;
+  }
+  function kmToMiles<T extends Vehicle>(vehicle: T): T {
+    vehicle.run = vehicle.run / 0.62;
+    return vehicle;
+  }
+
+  class LSV extends Vehicle {
+    capacity: number;
+  }
+  const vehicle = kmToMiles(new Vehicle());
+  const lsv = kmToMiles(new LSV());
+  const someVehicle = kmToMiles({ run: 1 });
+  // ------------------------------------------------------------------
+  function logId(id: string | number): string | number {
+    console.log(id);
+    return id;
+  }
+  function logId2<T extends string | number, Y>(id: T, additionalData: Y): { id: T; data: Y } {
+    console.log(id);
+    console.log(additionalData);
+    return { id, data: additionalData };
+  }
+}
+
 // ====== Generics in types =======
 
 {
